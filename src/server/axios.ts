fd537e48ac854892e8cccfd1e629372b370022da
Jirat Meewassana <64889751+JiMeow@users.noreply.github.com>
2023-11-10 05:20:36 +0700

    Update env

--- src/server/axios.ts
+++ src/server/axios.ts
@@ -2,6 +2,6 @@ import { env } from "@/env.mjs";
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: env.WEBSITE_URL
+    baseURL: env.NEXT_PUBLIC_API_URL
   });
   
